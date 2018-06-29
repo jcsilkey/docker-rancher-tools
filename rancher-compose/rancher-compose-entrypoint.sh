64da90f95fe4ee62bc689522a4b5465d91e4527a
Jason Silkey <jason@jasonsilkey.com>
2018-06-29 14:17:50 -0700

    check if required environment variables are set

--- rancher-compose/rancher-compose-entrypoint.sh
+++ rancher-compose/rancher-compose-entrypoint.sh
@@ -4,6 +4,20 @@ set -e
 
 envsubst < /rancher/rancher-compose.template.yml > /rancher/rancher-compose.yml
 envsubst < /rancher/docker-compose.template.yml > /rancher/docker-compose.yml
+rancher_env_vars="RANCHER_URL RANCHER_ACCESS_KEY RANCHER_SECRET_KEY RANCHER_ENVIRONMENT"
+
+missing_env_vars=0
+
+for rancher_env_var in $rancher_env_vars; do
+    if [ -z $(eval echo \$$rancher_env_var) ]; then
+        echo "${rancher_env_var} environment variable is required"
+        missing_env_vars=1
+    fi
+done
+
+if [ $missing_env_vars != 0 ]; then
+    exit 1
+fi
 
 set -- rancher-compose "$@"
 
