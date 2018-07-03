9701964f15424c64e4b10826c3bd5b86417945bd
Jason Silkey <jason@jasonsilkey.com>
2018-07-03 07:44:16 -0700

    refactor looking for compose templates

--- rancher-cli/rancher-entrypoint.sh
+++ rancher-cli/rancher-entrypoint.sh
@@ -2,26 +2,11 @@
 
 set -e
 
-rancher_env_vars="RANCHER_URL RANCHER_ACCESS_KEY RANCHER_SECRET_KEY RANCHER_ENVIRONMENT"
-
-missing_env_vars=0
-
-for rancher_env_var in $rancher_env_vars; do
-    if [ -z $(eval echo \$$rancher_env_var) ]; then
-        echo "${rancher_env_var} environment variable is required"
-        missing_env_vars=1
-    fi
-done
-
-if [ $missing_env_vars != 0 ]; then
-    exit 1
-fi
-
-templates="rancher-compose docker-compose"
+templates="rancher-compose.yml docker-compose.yml"
 
 for template in $templates; do
-    if [ -f "${template}.template.yml" ]; then
-        envsubst < "${template}.template.yml" > "${template}.yml"
+    if [ -f "templates/${template}" ]; then
+        envsubst < "templates/${template}" > "${template}"
     fi
 done
 
